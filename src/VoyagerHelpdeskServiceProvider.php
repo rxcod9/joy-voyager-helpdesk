cb132629518d633e71e66f1a0bfa14beaeb9479c
Ramakant Gangwar <gangwar.ramakant@gmail.com>
2023-01-23 01:56:17 +0530

    Manage autoload migrations

--- src/VoyagerHelpdeskServiceProvider.php
+++ src/VoyagerHelpdeskServiceProvider.php
@@ -34,7 +34,9 @@ class VoyagerHelpdeskServiceProvider extends ServiceProvider
 
         $this->mapWebRoutes();
 
-        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        if (config('joy-voyager-helpdesk.database.autoload_migrations', true)) {
+            $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+        }
 
         $this->loadTranslationsFrom(__DIR__ . '/../resources/lang', 'joy-voyager-helpdesk');
     }
